60ac9fcc4c20c95021a0ff29a16b64c842ed4f7d
EricLin0509 <ericlin050914@gmail.com>
2024-09-06 11:12:24 +0800

    Update note

--- 指针/2.指针讲解.c
+++ 指针/2.指针讲解.c
@@ -6,12 +6,13 @@ int main(int argc, const char * argv[] ) {
 
 int a = 20;
 int b = 100;
-int * p = &a;
+int * p = &a; // 将a的地址给到变量p
 p = &b; // 修改变量p存储的内存地址
-*p = 200;
+*p = 200; // 通过地址取值，同时改变了变量b对应的那块内存的值
 // 验证
+printf("a=%d\n",a);
 printf("b=%d\n",b);
-printf("p=%d\n",*p);
+printf("p=%d\n",*p); // 说明 *p 指向与b
 
 return 0;
 }
