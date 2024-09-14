89491776e08204cd6c4aade6bc8406fa0dee0aa0
EricLin0509 <ericlin050914@gmail.com>
2024-09-14 22:15:37 +0800

    Update note

--- 指针/补充：注意事项.c
+++ 指针/补充：注意事项.c
@@ -34,5 +34,33 @@ printf("sizeof(p2): %d\n",sizeof(p2));
 
 int *p4,*p5,*p6; // 推荐写法
 
+
+// array[] 和  &array[] 的区别
+int array[5];
+
+array[2] = 5;
+printf("array: %zu\n",array); // array 指向的是数组的第一个元素的内存
+printf("array+1: %zu\n",array+1); // 增加4个字节 (增加1个 int 类型)
+
+printf("&array: %zu\n",&array); // &array 指向的是整一个数组
+printf("&array+1: %zu\n\n",&array+1); // 增加20个字节 (增加5个 int 类型)(这个数组大小)
+
+// 二维数组
+int matrix[3][5] = {{0,1,2,3,4},{5,6,7,8,9},{10,11,12,13,14}};
+
+printf("matrix[1]: %zu\n",matrix[1]); // matrix[1] 指向第二个子数组里的第一个元素的内存
+printf("matrix[1]+1: %zu\n\n",matrix[1]+1); // 增加4个字节 (增加1个 int 类型)
+printf("*(matrix[1]+1): %zu\n\n",*(matrix[1]+1)); // 输出6
+
+printf("&matrix[1]: %zu\n",&matrix[1]); // &matrix[1] 指向整个第二个子数组
+printf("&matrix[1]+1: %zu\n",&matrix[1]+1); // 增加20个字节 (增加5个 int 类型)(里面子数组大小)
+printf("*(&matrix[1]+1): %zu\n\n",*(&matrix[1]+1)); // *(&matrix[1]+1) 指向第三个子数组里的第一个元素的内存
+
+int *pointer = (int *)(&matrix[1] +1);
+
+printf("pointer: %zu\n",pointer);
+printf("*pointer: %zu\n",*pointer); // 输出10
+printf("*(*(&matrix[1]+1)): %zu\n\n",*(*(&matrix[1]+1)));
+
 return 0;
 }
