036b66ff0411bb142cc7b48db07a0705aac7a59f
EricLin0509 <ericlin050914@gmail.com>
2024-09-21 18:01:40 +0800

    Add notes

--- 指针/15.函数指针数组.c
+++ 指针/15.函数指针数组.c
@@ -57,6 +57,8 @@ int (*Choice())(int,int)
     }
 }
 
+void test(const char* str) {}
+
 int main(int argc, const char * argv[]) {
 
 int (*pArr[2])(int,int) = {Plus,Minus}; // pArr  就是函数指针数组
@@ -67,6 +69,23 @@ scanf("%d",&a);
 scanf("%d",&b);
 int (*operation)(int,int) = Choice();
 printf("结果：%d\n",operation(a,b));
+
+
+int (*p)(int,int); // 函数指针
+int (* p2[4])(int, int); // 函数指针的数组
+// &p2: 取出的是函数指针数组的地址
+int (* (*p3)[4])(int, int) = &p2; // p3 就是一个指向 [函数指针的数组] 的指针
+printf("%zu\n",p3);
+
+// 函数指针pfun
+void (*pfun)(const char *) = test;
+
+//函数指针的数组pfunArr
+void (*pfunArr[5])(const char *str);
+pfunArr[0] = test;
+
+// 指向函数指针数组pfunArr的指针ppfunArr
+void (*(*ppfunArr)[5])(const char *) = &pfunArr;
 
 return 0;
 
---
+++ 指针/16.回调函数.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+bool Celsius(int temperature)
+{
+    if (temperature <= 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }   
+}
+
+bool Fahtenheit(int temperature)
+{
+    if (temperature <= 32)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void freezing(bool (*check)(int))
+{
+    int temperature = 0;
+    printf("输入温度：");
+    scanf("%d",&temperature);
+    if (check(temperature))
+    {
+        printf("会结冰\n");
+    }
+    else
+    {
+        printf("不会结冰\n");
+    }
+    
+}
+
+int main(int argc, const char * argv[]) {
+
+int option = 0;
+printf("选择单位\n");
+printf("1. 摄氏度 ℃\n");
+printf("2. 华氏度 ℉\n");
+printf("请选择：");
+scanf("%d",&option);
+
+switch (option)
+{
+case 1:
+    freezing(Celsius);
+    break;
+
+case 2:
+    freezing(Fahtenheit);
+    break;
+
+default:
+    printf("输入错误，请重试\n");
+    break;
+}
+
+return 0;
+
+}
+
